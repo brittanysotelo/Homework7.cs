dc2ac5f7a0eb3a7ee1a407fa7f693077e760ead9
brittanysotelo <112566764+brittanysotelo@users.noreply.github.com>
2022-10-19 23:36:25 -0500

    Update Homework7.cs

--- Homework7.cs
+++ Homework7.cs
@@ -27,3 +27,24 @@ class Program
     //     C2.CompareAge(C1);
     }
 }
+
+class Customer{
+    public int ID;
+    public string Name;
+    public int Age;
+    public void PrintCustomerInfo(){
+        Console.WriteLine($"Customer: {ID}, name: {Name}, age: {Age}");
+    }
+    public int new_ID;
+    public void UpdateNew_ID(){
+        Console.WriteLine($"Customer: {new_ID}, name: {Name}, age: {Age}");
+    }
+    //Wasn't sure on how to do the last part, this is what I got to
+    // public void CompareAge(Customer objCustomer){
+    //     if(Age <= objCustomer.Age){
+    //         Console.WriteLine($"{Age} is older");
+    //     }else{
+    //         Console.WriteLine($"{objCustomer} is older");
+    //   }
+    // }
+}
